d6ef01bf33344d57f024d32696452d47330af6f6
VinylKnight <roger.bucio@loop.colum.edu>
2018-10-03 14:23:19 -0500

    Merge branch 'master' of https://github.com/VinylKnight/DangerDemon

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -8,21 +8,24 @@ public class PlayerCharacter : MonoBehaviour {
 
     [SerializeField]
     private int lives = 3;
-
     [SerializeField]
     private string Name = "Mario";
     [SerializeField]
-    private float jumpHeight = 5, speed = 5;
-    
-    private bool hasKey;
-    
-    private bool isOnGround;
+    private float jumpHeight = 5, accelerationForce = 5;
+    [SerializeField]
     private Rigidbody2D rigidbody2DInstance;
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    private bool hasKey;
+    private bool isOnGround;
     private float horizontalInput;
+
+    
     // Use this for initialization
     void Start () {
 
-        rigidbody2DInstance = GetComponent<Rigidbody2D>();
+        //rigidbody2DInstance = GetComponent<Rigidbody2D>();
         
 	}
 	
@@ -30,22 +33,32 @@ public class PlayerCharacter : MonoBehaviour {
 	void Update () {
 
         //transform.Translate( 0, -.01f,  0); not physics based do not use
-        GetInput();
-        Move();
+        //GetInput();
+        //Move();
+        horizontalInput = Input.GetAxis("Horizontal");
 	}
 
-    private void GetInput()
-    {
+    //private void GetInput()
+    //{
 
-        float horizontalInput = Input.GetAxis("Horizontal");
+    //    float horizontalInput = Input.GetAxis("Horizontal");
 
 
-    }
+    //}
 
-    private void Move()
-    {
-        rigidbody2DInstance.velocity = new Vector2(horizontalInput, 0);
+    //private void Move()
+    //{
+    //    rigidbody2DInstance.velocity = new Vector2(horizontalInput, -1);
         
+
+    //}
+    private void FixedUpdate()
+    {
+        rigidbody2DInstance.AddForce(Vector2.right * horizontalInput * accelerationForce);
+        Vector2 clampedVelocity = rigidbody2DInstance.velocity;
+        clampedVelocity = Mathf.Clamp(rigidbody2DInstance.velocity.x, -maxSpeed, maxSpeed);
+        rigidbody2DInstance.velocity = clampedVelocity;
+
 
     }
 
