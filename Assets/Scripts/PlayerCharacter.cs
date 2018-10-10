770253d1236c583c318a0b891d8e0a3c683de97b
Roger Bucio <Roger.Bucio@iam.local>
2018-10-10 14:12:51 -0500

    Jump and Material addition

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -13,36 +13,52 @@ public class PlayerCharacter : MonoBehaviour {
     private Rigidbody2D rigidbody2DInstance;
     [SerializeField]
     private float maxSpeed = 5;
+    [SerializeField]
+    private float jumpForce = 5;
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
 
-    
+    private bool isOnGround;
+
     private float horizontalInput;
 
-    
-    // Use this for initialization
-    void Start () {
-
-        //rigidbody2DInstance = GetComponent<Rigidbody2D>();
-        
-	}
-	
 	// Update is called once per frame
-	void Update () {
-
+	void Update ()
+    {
         //transform.Translate( 0, -.01f,  0); not physics based do not use
-       
-        horizontalInput = Input.GetAxis("Horizontal");
-	}
 
-   
+        UpdateHorizontalInput();
+        HandleJumpInput();
+
+    }
+
+    private void UpdateHorizontalInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+
+            rigidbody2DInstance.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+        }
+    }
+
     private void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    private void Move()
     {
         rigidbody2DInstance.AddForce(Vector2.right * horizontalInput * accelerationForce);
         Vector2 clampedVelocity = rigidbody2DInstance.velocity;
         clampedVelocity.x = Mathf.Clamp(rigidbody2DInstance.velocity.x, -maxSpeed, maxSpeed);
         rigidbody2DInstance.velocity = clampedVelocity;
-        
-
     }
-
-
 }
