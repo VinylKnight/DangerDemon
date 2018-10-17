5e2c7e4001531c5049d8f9b4056f5b82b7d1b55b
Roger Bucio <Roger.Bucio@iam.local>
2018-10-17 14:09:01 -0500

    Hazards and Reloading Scenes

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Hazard : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+        else {
+
+
+
+
+        }
+    }
+}
