b69db4e6bc6c135f6ccd4c27549d0cba15a3e604
VinylKnight <roger.bucio@loop.colum.edu>
2018-10-30 23:42:05 -0500

    Checkpoint feedback Part 1

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -3,7 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
+    [SerializeField]
+    private float inactiveRotationSpeed = 100, activeRotationSpeed = 200;
 
+    private bool isActivated;
+    private void Update()
+    {
+        UpdateRotation();
+
+
+
+    }
+    private void UpdateRotation()
+    {
+        transform.Rotate(Vector3.up * inactiveRotationSpeed * Time.deltaTime);
+
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -11,6 +27,7 @@ public class Checkpoint : MonoBehaviour {
 
             PlayerCharacter player = collision.GetComponent<PlayerCharacter>();
             player.SetCurrentCheckpoint(this);
+            isActivated = true;
 
         }
        
